b3e7eb4a298507e9bf3a335a01c502ca9c99867a
Dante <Mill8622@morris.umn.edu>
2021-03-28 03:18:40 -0600

    Create Components
    Created the components for add wordlists and put it in the app module

---
+++ client/src/app/wordlists/add-wordlists/add-wordlists.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddWordlistsComponent } from './add-wordlists.component';
+
+describe('AddWordlistsComponent', () => {
+  let component: AddWordlistsComponent;
+  let fixture: ComponentFixture<AddWordlistsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddWordlistsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddWordlistsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/wordlists/add-wordlists/add-wordlists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-wordlists',
+  templateUrl: './add-wordlists.component.html',
+  styleUrls: ['./add-wordlists.component.scss']
+})
+export class AddWordlistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
