b620e4ccb102dca12be1e6e4f2139ffb9f3c3b42
Dante <Mill8622@morris.umn.edu>
2021-04-03 00:32:50 -0600

    Delete Tests, Space, and HTML Code
    Deleted tests related to the display wordlist method, which i got rid of a while ago. Deleted unused space and code that is not needed.

--- client/src/app/contextpacks/contextpack-card.component.spec.ts
+++ client/src/app/contextpacks/contextpack-card.component.spec.ts
@@ -119,27 +119,6 @@ describe('ContextPackCardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should list the nouns, verbs, adjectives and misc words when displayWordlist() is called', () => {
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('you, yoyo, yos, yoted');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('green, greener');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('ran, running');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).toContain('langerhans, langerhan');
-    expect(component.displayWordlists(component.contextpack.wordlists[0])).not.toContain('barbie');
-  });
-
-  it('should return the nouns displayAllNouns() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'nouns')).toContain('you, yoyo, yos, yoted, you, yoyo, yos, yoted');
-  });
-  it('should return the verbs when displayAllVerbs() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'verbs')).toContain('ran, running, ran, running');
-  });
-  it('should return the adjectives when displayAllAdjectives() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'adjectives')).toContain('green, greener, green, greener');
-  });
-  it('should return the misc words when displayAllMisc() is called', () => {
-    expect(component.displayAllWords(component.contextpack, 'misc')).toContain('langerhans, langerhan, langerhans, langerhan');
-  });
-
 
   it('should have displayNouns,verbs,adjective,misc return null if undefined', () => {
     expect(component.displayWords(emptyWordlist, 'nouns')).toBeNull();
